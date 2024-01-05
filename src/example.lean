10bb4596f0b0725d18365191b470022897a2c26d
Fady Adal <2masadel@gmail.com>
2024-01-05 15:48:32 -0600

    add division theorem

--- src/example.lean
+++ src/example.lean
@@ -4,3 +4,6 @@ theorem hello_world (a b c : ℕ) : a + b + c = a + c + b :=
 begin
   rw [add_assoc, add_comm b, ←add_assoc]
 end
+
+theorem division_theorem (a b : ℕ) (h : b > 0) : ∃ q r : ℕ, a = b * q + r ∧ r < b :=
+by admit
